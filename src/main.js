068c0ceac3404096c064af223378e14aa9416a16
MinghanHhh <72732534+MinghanHhh@users.noreply.github.com>
2021-01-15 11:25:27 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import '../src/assets/fonts/iconfont.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//这样的话每一个vue组件都可以通过this直接访问访问http发起Ajax的请求
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,17 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// import { Button } from 'element-ui'
+// import { Form, FormItem } from 'element-ui'
+// import { Input } from 'element-ui'
+// //导入弹框提示组件
+// import { Message} from 'element-ui'
+
+//将上面的合并为一行
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//把弹框组件挂在到了vue的原型对象上,每一个组件都可以通过this访问$message,就可以实现弹框提示了
+Vue.prototype.$message = Message
+
